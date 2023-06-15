aeb48d9c3d7b4d01be3290ffbdd9a6bc7eb33400
sulleimenov <sulleimenov@yandex.ru>
2023-06-15 18:52:19 +0600

    add home page blocks

--- app/js/app.js
+++ app/js/app.js
@@ -1,45 +1,24 @@
 // import '~/app/vendor/mmenu/dist/mmenu.js'
 
+import Swiper, { Navigation, Pagination } from 'swiper';
+
 document.addEventListener('DOMContentLoaded', () => {
-	/* lazy */
-	const lazyImages = document.querySelectorAll('img[data-src],source[data-srcset]');
-	const loadMapBlock = document.querySelector('.load-map');
-	const windowHeight = document.documentElement.clientHeight;
+	const swiper = new Swiper('.client__slider', {
+		// Optional parameters
+		modules: [Navigation, Pagination],
 
-	let lazyImagesPositions = [];
-	if (lazyImages.length > 0) {
-		lazyImages.forEach((img) => {
-			if (img.dataset.src || img.dataset.srcset) {
-				lazyImagesPositions.push(img.getBoundingClientRect().top + pageYOffset);
-			}
-		});
-	}
-	console.log(lazyImagesPositions);
+		loop: true,
+		autoplay: true,
 
-	window.addEventListener('scroll', lazyScroll);
+		// If we need pagination
+		pagination: {
+			el: '.swiper-pagination',
+		},
 
-	function lazyScroll() {
-		if (document.querySelectorAll('img[data-src], source[data-srcset').length > 0) {
-			lazyLoadCheck();
-		}
-	}
-
-	function lazyLoadCheck() {
-		let imgIndex = lazyImagesPositions.findIndex((item) => pageYOffset > item - windowHeight);
-		if (imgIndex >= 0) {
-			if (lazyImages[imgIndex].dataset.src) {
-				lazyImages[imgIndex].src = lazyImages[imgIndex].dataset.src;
-				lazyImages[imgIndex].removeAttribute('data-src');
-			} else if (lazyImages[imgIndex].dataset.srcset) {
-				lazyImages[imgIndex].srcset = lazyImages[imgIndex].dataset.srcset;
-				lazyImages[imgIndex].removeAttribute('data-srcset');
-			}
-			delete lazyImagesPositions[imgIndex];
-		}
-	}
-
-	// const images = document.querySelectorAll('img');
-	// images.forEach((image) => {
-	// 	image.classList.add('lazy');
-	// });
+		// Navigation arrows
+		navigation: {
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev',
+		},
+	});
 });
